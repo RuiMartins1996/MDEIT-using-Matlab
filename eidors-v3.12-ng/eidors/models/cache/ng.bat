0363a4d3e180e31a4c352b7179eaf7e4274ab376
Rui Martins <martins.rui@ua.pt>
2026-01-27 16:22:50 +0000

    Modified EIDORS

--- eidors-v3.12-ng/eidors/models/cache/ng.bat
+++ eidors-v3.12-ng/eidors/models/cache/ng.bat
@@ -1,4 +1,4 @@
-set TCL_LIBRARY=C:/Users/RuiMartins/Desktop/SVD Comparison/eidors-v3.12-ng/eidors/../Netgen-5.3_x64/bin/lib/tcl8.3
-set TIX_LIBRARY=C:/Users/RuiMartins/Desktop/SVD Comparison/eidors-v3.12-ng/eidors/../Netgen-5.3_x64/bin/lib/tix8.1
-set NETGENDIR=C:/Users/RuiMartins/Desktop/SVD Comparison/eidors-v3.12-ng/eidors/../Netgen-5.3_x64/bin
-"C:/Users/RuiMartins/Desktop/SVD Comparison/eidors-v3.12-ng/eidors/../Netgen-5.3_x64/bin/netgen.exe" %*
+set TCL_LIBRARY=C:/Users/RuiMartins/Desktop/MDEIT-using-Matlab-main/eidors-v3.12-ng/eidors/../Netgen-5.3_x64/bin/lib/tcl8.3
+set TIX_LIBRARY=C:/Users/RuiMartins/Desktop/MDEIT-using-Matlab-main/eidors-v3.12-ng/eidors/../Netgen-5.3_x64/bin/lib/tix8.1
+set NETGENDIR=C:/Users/RuiMartins/Desktop/MDEIT-using-Matlab-main/eidors-v3.12-ng/eidors/../Netgen-5.3_x64/bin
+"C:/Users/RuiMartins/Desktop/MDEIT-using-Matlab-main/eidors-v3.12-ng/eidors/../Netgen-5.3_x64/bin/netgen.exe" %*
