83b5d8ea8b4be214159ff346737751838a47ae64
Rui Martins <martins.rui@ua.pt>
2026-01-28 16:33:14 +0000

    Changed the name of Gamma matrices from GammaX,GammaY,GammaZ to Gamma1,Gamma2,Gamma3

--- functions/fwd_model/fwd_solve_mdeit.m
+++ functions/fwd_model/fwd_solve_mdeit.m
@@ -12,9 +12,9 @@ end
 %% Output
 img = compute_gamma_matrices(img);
 
-Bx = img.GammaX*u.volt;
-By = img.GammaY*u.volt;
-Bz = img.GammaZ*u.volt;
+Bx = img.Gamma1*u.volt;
+By = img.Gamma2*u.volt;
+Bz = img.Gamma3*u.volt;
 
 data = struct('Bx',Bx,'By',By,'Bz',Bz);
 end
