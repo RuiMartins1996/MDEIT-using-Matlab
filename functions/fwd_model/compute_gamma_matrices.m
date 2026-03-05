7b869fa87aaac0471b557e0564749227b6626ffd
Rui Martins <martins.rui@ua.pt>
2026-03-05 16:37:19 +0000

    Made changes to these functions so they are more efficient, implementing the changes I had worked on on a different script while working on the optimization of sensor configuration. In particuar, in calc_jacobian, a permutation is no longer needed at the end, and the adjoint system is solved with backslash and not pcg, which is faster

--- functions/fwd_model/compute_gamma_matrices.m
+++ functions/fwd_model/compute_gamma_matrices.m
@@ -37,39 +37,6 @@ img.Gamma1 = Gamma1;
 img.Gamma2 = Gamma2;
 img.Gamma3 = Gamma3;
 
-
-
-
-
-
-% % Check for existing fields and reuse if available
-% if isfield(img, 'GammaX') && ~isempty(img.GammaX)
-%     if not(isequal(size(img.GammaX), expectedSize))
-%         error('Expected size for Gamma matrix is wrong')
-%     end
-% else
-%     GammaX = mu_factor * ( -R.Rz * Sigma * G.Gy +  R.Ry * Sigma * G.Gz );
-%     img.GammaX = GammaX;
-% end
-% 
-% if isfield(img, 'GammaY') && ~isempty(img.GammaY)
-%     if not(isequal(size(img.GammaY), expectedSize))
-%         error('Expected size for Gamma matrix is wrong')
-%     end
-% else
-%     GammaY = mu_factor * ( -R.Rx * Sigma * G.Gz +  R.Rz * Sigma * G.Gx );
-%     img.GammaY = GammaY;
-% end
-% 
-% if isfield(img, 'GammaZ') && ~isempty(img.GammaZ)
-%     if not(isequal(size(img.GammaZ), expectedSize))
-%         error('Expected size for Gamma matrix is wrong')
-%     end
-% else
-%     GammaZ = mu_factor * ( -R.Ry * Sigma * G.Gx +  R.Rx * Sigma * G.Gy );
-%     img.GammaZ = GammaZ;
-% end
-
 end
 
 
