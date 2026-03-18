d0286841afd32a8263f4ea973015723287562f3b
Rui Martins <martins.rui@ua.pt>
2026-03-18 18:02:23 +0000

    Added way to handle if the noise generator function generates one sample at the time, or all the samples at once

--- functions/noise_correction.m
+++ functions/noise_correction.m
@@ -7,13 +7,31 @@ if nargin <7
     [U,S,V] = svd(Jh,'econ');
 end
 
-data_noisy = noisy_data_generator(imgh,imgi,num_noise_repetitions);
+try % noisy data generator generates all samples once
+    data_noisy = noisy_data_generator(imgh,imgi,num_noise_repetitions);
+    
+    s  = diag(S);
+    sv = s + lambda./s;
+    M = V * diag(1./sv) * U' * data_noisy;
 
-s  = diag(S);
-sv = s + lambda./s;
-M = V * diag(1./sv) * U' * data_noisy;
+    sigma_std = std(M,[],2);
+catch % noisy data generator generates a sample each time
+    
 
-sigma_std = std(M,[],2);
+    s  = diag(S);
+    sv = s + lambda./s;
+    
+    M = zeros(size(Jh,2),num_noise_repetitions);
+
+    for i = 1:num_noise_repetitions
+        data_noisy = noisy_data_generator(imgh,imgi);
+        M(:,i) = V * diag(1./sv) * U' * data_noisy;
+    end
+
+    sigma_std = std(M,[],2);
+end
+
 
 end
+
 
